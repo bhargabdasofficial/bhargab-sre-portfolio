44a3a41baeab7c3f356bf2ec920f52511f145d03
bhargabdas <bhargabdasofficial@gmail.com>
2025-08-23 01:14:12 +0530

    Week1: Made some modification to for_loop_eg.sh with addition comments

--- Week-1-Linux-Bash/bash_scripts/for_loop_eg.sh
+++ Week-1-Linux-Bash/bash_scripts/for_loop_eg.sh
@@ -5,8 +5,15 @@ DIR=($(pwd)) # Looks for file in current working dir
 
 echo "Checking files under current working directory :$DIR"
 
+
+# commenting previous script
+#for file in "$DIR"/*; do
+#       echo "File: $file"
+#done 
+
+# update for loop for more clarity
 for file in "$DIR"/*; do
-    if [ -f "$file" ]; then
-       echo "File: $(basename $file)"
+    if [ -f "$file" ]; then # -f tests if the given path is a regular file (not a directory, not a symlink)
+       echo "File: $(basename $file)" # extracts just the filename without the path
     fi
 done 
